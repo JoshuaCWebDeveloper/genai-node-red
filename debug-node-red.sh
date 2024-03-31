f0a2120db26c6cc84006b53f740b0bd63c750038
Joshua Carter <carter.joshua.603@gmail.com>
2024-03-30 20:18:25 -0700

    Create new bash scripts for running node-red

---
+++ debug-node-red.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker run -e NODE_OPTIONS="--inspect=0.0.0.0:9229" --rm -it -p 1880:1880 -p 9229:9229 -v $(pwd)/data:/data --name mynodered nodered/node-red 
+read -n 1 -s -r -p "Press any key to continue"
---
+++ node-red.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker run --rm -it -p 1880:1880 -v $(pwd)/data:/data --name mynodered nodered/node-red:3.1.3-18
+read -n 1 -s -r -p "Press any key to continue"
