b6a180a476937c1e60e88943272daa70a36eee07
Joshua Carter <carter.joshua.603@gmail.com>
2024-04-05 12:48:03 -0700

    Move existing packages into nx packages/ directory

--- debug-node-red.bat
+++ debug-node-red.bat
@@ -1,2 +1,2 @@
-docker run -e NODE_OPTIONS="--inspect=0.0.0.0:9229" --rm -it -p 1880:1880 -p 9229:9229 -v C:\Users\carte\Documents\WebDev\projects\gen-ai\node-red\data:/data --name mynodered nodered/node-red 
+docker run -e NODE_OPTIONS="--inspect=0.0.0.0:9229" --rm -it -p 1880:1880 -p 9229:9229 -v C:\Users\carte\Documents\WebDev\projects\gen-ai\node-red\packages\node-red-data:/data --name mynodered nodered/node-red 
 pause
--- node-red.bat
+++ node-red.bat
@@ -1,2 +1,2 @@
-docker run --rm -it -p 1880:1880 -v %~dp0data:/data --name mynodered nodered/node-red:3.1.3-18
+docker run --rm -it -p 1880:1880 -v %~dp0packages\node-red-data:/data --name mynodered nodered/node-red:3.1.3-18
 pause
