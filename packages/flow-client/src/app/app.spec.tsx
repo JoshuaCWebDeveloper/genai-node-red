a1451e4579f852bf27ac62754365b801a364da92
Joshua Carter <carter.joshua.603@gmail.com>
2024-04-07 20:35:29 -0700

    Github CI/CD (#7)
    * Create new Github ci/cd workflow
    
    * Update workflow
    
    * Set nx base to origin/master
    
    * Switch project to type: module
    
    * Introduce lint error
    
    * Add lint and build jobs to workflow
    
    * Make lint error worse
    
    * Make lint error worse
    
    * Introduce substantial change
    
    * Introduce additional errors
    
    * Fix format:check command
    
    * Fix lint and format errors
    
    * Set tsconfig module and target for flow-client
    
    * Fix app.spec test
    
    * Fix app.spec test

--- packages/flow-client/src/app/app.spec.tsx
+++ packages/flow-client/src/app/app.spec.tsx
@@ -1,15 +1,54 @@
 import { render } from '@testing-library/react';
 
-import App from './app';
+vi.stubEnv('VITE_NODE_RED_API_ROOT', 'https://www.example.com/api');
+
+const { default: App } = await import('./app');
+const { AppProvider, createLogic } = await import('./redux/logic');
+const { createStore } = await import('./redux/store');
+
+// Mock the API queries
+vi.mock('./redux/modules/api/node.api', async importOriginal => {
+    const originalModule = await importOriginal<
+        typeof import('./redux/modules/api/node.api')
+    >();
+    const nodesData = [{ id: '1', name: 'Test Node' }];
+    const nodeScriptsData = [
+        { id: '1', script: 'console.log("test script");' },
+    ];
+    return {
+        ...originalModule,
+        useGetNodesQuery: vi.fn(() => ({
+            data: nodesData,
+            error: null,
+            isLoading: false,
+        })),
+        useGetNodeScriptsQuery: vi.fn(() => ({
+            data: nodeScriptsData,
+            error: null,
+            isLoading: false,
+        })),
+    };
+});
+
+vi.mock('./components/flow-canvas-container', () => ({
+    FlowCanvasContainer: () => <div>Flow Container</div>,
+}));
 
 describe('App', () => {
+    const renderApp = () =>
+        render(
+            <AppProvider store={createStore()} logic={createLogic()}>
+                <App />
+            </AppProvider>
+        );
+
     it('should render successfully', () => {
-        const { baseElement } = render(<App />);
+        const { baseElement } = renderApp();
         expect(baseElement).toBeTruthy();
     });
 
     it('should have a greeting as the title', () => {
-        const { getByText } = render(<App />);
-        expect(getByText(/Welcome react-2024-01/gi)).toBeTruthy();
+        const { getByText } = renderApp();
+        expect(getByText(/Flow Canvas/gi)).toBeTruthy();
     });
 });
