5691b5504e7be7bfad3e5f6e816d55d9ad16f024
Joshua Carter <carter.joshua.603@gmail.com>
2024-04-09 07:47:32 -0700

    Move CustomEngine to separate file

---
+++ packages/flow-client/src/app/components/flow-canvas/custom-engine.ts
@@ -0,0 +1,75 @@
+import {
+    AbstractReactFactory,
+    DefaultDiagramState,
+    DefaultLabelFactory,
+    DefaultLinkFactory,
+    DefaultNodeFactory,
+    DefaultPortFactory,
+    DiagramEngine,
+    LayerModel,
+    LayerModelGenerics,
+    LinkLayerFactory,
+    NodeLayerFactory,
+    PathFindingLinkFactory,
+    SelectionBoxLayerFactory,
+} from '@projectstorm/react-diagrams';
+
+export class CustomEngine extends DiagramEngine {
+    public increaseZoomLevel(event: WheelEvent): void {
+        const model = this.getModel();
+        if (model) {
+            const zoomFactor = 0.1; // Adjust this value based on your needs
+            const oldZoomLevel = model.getZoomLevel();
+            const newZoomLevel = Math.max(
+                oldZoomLevel + event.deltaY * -zoomFactor,
+                50
+            );
+
+            // Calculate the new offset
+            const boundingRect = (
+                event.currentTarget as Element
+            )?.getBoundingClientRect();
+            const clientX = event.clientX - boundingRect.left;
+            const clientY = event.clientY - boundingRect.top;
+            const deltaX = clientX - model.getOffsetX();
+            const deltaY = clientY - model.getOffsetY();
+            const zoomRatio = newZoomLevel / oldZoomLevel;
+            const newOffsetX = clientX - deltaX * zoomRatio;
+            const newOffsetY = clientY - deltaY * zoomRatio;
+
+            model.setZoomLevel(newZoomLevel);
+            model.setOffset(newOffsetX, newOffsetY);
+            this.repaintCanvas();
+        }
+    }
+}
+
+export const createEngine = (options = {}) => {
+    const engine = new CustomEngine(options);
+    // register model factories
+    engine
+        .getLayerFactories()
+        .registerFactory(
+            new NodeLayerFactory() as unknown as AbstractReactFactory<
+                LayerModel<LayerModelGenerics>,
+                DiagramEngine
+            >
+        );
+    engine
+        .getLayerFactories()
+        .registerFactory(
+            new LinkLayerFactory() as unknown as AbstractReactFactory<
+                LayerModel<LayerModelGenerics>,
+                DiagramEngine
+            >
+        );
+    engine.getLayerFactories().registerFactory(new SelectionBoxLayerFactory());
+    engine.getLabelFactories().registerFactory(new DefaultLabelFactory());
+    engine.getNodeFactories().registerFactory(new DefaultNodeFactory()); // i cant figure out why
+    engine.getLinkFactories().registerFactory(new DefaultLinkFactory());
+    engine.getLinkFactories().registerFactory(new PathFindingLinkFactory());
+    engine.getPortFactories().registerFactory(new DefaultPortFactory());
+    // register the default interaction behaviours
+    engine.getStateMachine().pushState(new DefaultDiagramState());
+    return engine;
+};
--- packages/flow-client/src/app/components/flow-canvas/flow-canvas-container.tsx
+++ packages/flow-client/src/app/components/flow-canvas/flow-canvas-container.tsx
@@ -1,27 +1,13 @@
+import { CanvasWidget } from '@projectstorm/react-canvas-core';
 import {
-    AbstractReactFactory,
-    CanvasWidget,
-    LayerModel,
-    LayerModelGenerics,
-    SelectionBoxLayerFactory,
-} from '@projectstorm/react-canvas-core';
-import {
-    DefaultDiagramState,
-    DefaultLabelFactory,
-    DefaultLinkFactory,
     DefaultLinkModel,
-    DefaultNodeFactory,
     DefaultNodeModel,
-    DefaultPortFactory,
     DiagramModel,
-    LinkLayerFactory,
-    NodeLayerFactory,
-    PathFindingLinkFactory,
 } from '@projectstorm/react-diagrams';
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
-import { DiagramEngine } from '@projectstorm/react-diagrams';
+import { createEngine } from './custom-engine';
 
 const StyledCanvasWidget = styled(CanvasWidget)`
     background-color: #f0f0f0; /* Light grey background */
@@ -31,66 +17,6 @@ const StyledCanvasWidget = styled(CanvasWidget)`
     ); /* Adjust height if you have a header or toolbar */
     overflow: auto; /* Allows scrolling within the canvas */
 `;
-
-class CustomEngine extends DiagramEngine {
-    public increaseZoomLevel(event: WheelEvent): void {
-        const model = this.getModel();
-        if (model) {
-            const zoomFactor = 0.1; // Adjust this value based on your needs
-            const oldZoomLevel = model.getZoomLevel();
-            const newZoomLevel = Math.max(
-                oldZoomLevel + event.deltaY * -zoomFactor,
-                50
-            );
-
-            // Calculate the new offset
-            const boundingRect = (
-                event.currentTarget as Element
-            )?.getBoundingClientRect();
-            const clientX = event.clientX - boundingRect.left;
-            const clientY = event.clientY - boundingRect.top;
-            const deltaX = clientX - model.getOffsetX();
-            const deltaY = clientY - model.getOffsetY();
-            const zoomRatio = newZoomLevel / oldZoomLevel;
-            const newOffsetX = clientX - deltaX * zoomRatio;
-            const newOffsetY = clientY - deltaY * zoomRatio;
-
-            model.setZoomLevel(newZoomLevel);
-            model.setOffset(newOffsetX, newOffsetY);
-            this.repaintCanvas();
-        }
-    }
-}
-
-const createEngine = (options = {}) => {
-    const engine = new CustomEngine(options);
-    // register model factories
-    engine
-        .getLayerFactories()
-        .registerFactory(
-            new NodeLayerFactory() as unknown as AbstractReactFactory<
-                LayerModel<LayerModelGenerics>,
-                DiagramEngine
-            >
-        );
-    engine
-        .getLayerFactories()
-        .registerFactory(
-            new LinkLayerFactory() as unknown as AbstractReactFactory<
-                LayerModel<LayerModelGenerics>,
-                DiagramEngine
-            >
-        );
-    engine.getLayerFactories().registerFactory(new SelectionBoxLayerFactory());
-    engine.getLabelFactories().registerFactory(new DefaultLabelFactory());
-    engine.getNodeFactories().registerFactory(new DefaultNodeFactory()); // i cant figure out why
-    engine.getLinkFactories().registerFactory(new DefaultLinkFactory());
-    engine.getLinkFactories().registerFactory(new PathFindingLinkFactory());
-    engine.getPortFactories().registerFactory(new DefaultPortFactory());
-    // register the default interaction behaviours
-    engine.getStateMachine().pushState(new DefaultDiagramState());
-    return engine;
-};
 
 const engine = createEngine();
 
