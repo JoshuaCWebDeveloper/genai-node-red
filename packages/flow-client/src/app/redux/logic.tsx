d6df46744a6944bbcbdeba76c8e73abc8ddb6b04
Joshua Carter <carter.joshua.603@gmail.com>
2024-04-18 14:48:16 -0700

    Correct redux-persist import

--- packages/flow-client/src/app/redux/logic.tsx
+++ packages/flow-client/src/app/redux/logic.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import persistStore from 'redux-persist';
+import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { FeatureLogic } from './modules/feature/feature.logic';
