3792d13fe0d68b7a33716564b54e1bd30e7a2d10
Joshua Carter <carter.joshua.603@gmail.com>
2024-04-30 08:19:01 -0700

    Dispatch node actions directly from api query instead of node-palette component

--- packages/flow-client/src/app/components/node-palette/node-palette.tsx
+++ packages/flow-client/src/app/components/node-palette/node-palette.tsx
@@ -1,12 +1,6 @@
-import { useEffect } from 'react';
 import styled from 'styled-components';
-// Replace the following lines
-// import { useDispatch, useSelector } from 'react-redux';
-import { useAppDispatch, useAppLogic, useAppSelector } from '../../redux/hooks';
-import {
-    useGetNodeScriptsQuery,
-    useGetNodesQuery,
-} from '../../redux/modules/api/node.api';
+
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import {
     nodeActions,
     selectFilteredNodes,
@@ -22,44 +16,11 @@ const StyledNodePalette = styled.div`
 
 export const NodePalette = () => {
     const dispatch = useAppDispatch();
-    const nodeLogic = useAppLogic().node;
-
-    const { data: rawNodes, error, isLoading } = useGetNodesQuery();
-    const {
-        data: nodeScripts,
-        error: nodeScriptsError,
-        isLoading: nodeScriptsLoading,
-    } = useGetNodeScriptsQuery();
-
-    useEffect(() => {
-        if (rawNodes) {
-            dispatch(nodeActions.setNodes(rawNodes));
-        }
-    }, [dispatch, rawNodes]);
-
-    useEffect(() => {
-        if (nodeScripts) {
-            dispatch(nodeLogic.setNodeScripts(nodeScripts));
-        }
-    }, [dispatch, nodeScripts, nodeLogic]);
-
-    useEffect(() => {
-        if (error) {
-            console.error('GetNodes error: ', error);
-        }
-        if (nodeScriptsError) {
-            console.error('GetNodeScripts error: ', nodeScriptsError);
-        }
-    }, [error, nodeScriptsError]);
 
     const nodes = useAppSelector(selectFilteredNodes);
     const handleSearch = (query: string) => {
         dispatch(nodeActions.setSearchQuery(query));
     };
-
-    if (isLoading || nodeScriptsLoading) return <p>Loading...</p>;
-    if (error || nodeScriptsError)
-        return <p className="error">Request error, please try again.</p>;
 
     return (
         <StyledNodePalette className="node-palette">
