136b83e51d74c6475dd9510e12294ac58fb047fc
Joshua Carter <carter.joshua.603@gmail.com>
2024-06-13 08:34:02 -0700

    Create new admin-operator flow (#2)
    * Create new admin-operator flow
    
    * Move ui-builder/admin-operator from old data directory to new packages/node-red-data
    
    * Format dummy-sse-app.js
    
    * Rename dummy-sse-app.js to dummy-sse-app.cjs
    
    * Install express and body-parser dev dependencies
    
    * Archive March session of experimenting with ubuntu commands
    
    * Update dummy-see-app response with tags for testing admin-op
    
    * Add host.docker.internal.local domain name that points to process IP address
    
    * Update flows.json with bug fixes for the admin-op flow, currently working

--- node-red.sh
+++ node-red.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -it -p 1880:1880 -v $(pwd)/packages/node-red-data:/data --name mynodered nodered/node-red:3.1.3-18
+docker run --rm -it --add-host host.docker.internal.local=$(hostname -I | awk '{print $1}') -p 1880:1880 -v $(pwd)/packages/node-red-data:/data --name mynodered nodered/node-red:3.1.3-18
 read -n 1 -s -r -p "Press any key to continue"
