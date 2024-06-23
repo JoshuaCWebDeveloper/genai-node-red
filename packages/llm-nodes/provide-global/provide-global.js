b9bd648da5a3dd8456baab4c54c4fd403df137b1
Joshua Carter <carter.joshua.603@gmail.com>
2024-06-22 18:19:57 -0700

    Correct misnamed provide-global.js and provide-import-fn.js

--- packages/llm-nodes/provide-global/provide-global.js
+++ packages/llm-nodes/provide-global/provide-global.js
@@ -1,22 +1,18 @@
 module.exports = (RED) => {
-    class ProvideImportFn {
+    class ProvideGlobal {
         constructor(config) {
             RED.nodes.createNode(this, config);
             this.on('input', (msg) => {
                 try {
-                    // Custom function wrapper logic
-                    const sendFunction = async (module) => {
-                        return await import(module);    
-                    }
-                    // Attach the function to the message object
-                    msg.import = sendFunction;
+                    // Attach global the message object
+                    msg.global = global;
                     // Send the updated message object
                     this.send(msg);
                 } catch (err) {
-                    this.error(`Error in ProvideImportFn: ${err.message}`, msg);
+                    this.error(`Error in ProvideGlobal: ${err.message}`, msg);
                 }
             });
         }
     }
-    RED.nodes.registerType("provide-import-fn", ProvideImportFn);
+    RED.nodes.registerType("provide-global", ProvideGlobal);
 };
