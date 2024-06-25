17cf9eb2ab96c8719591beb5e5a13961b04466f4
Joshua Carter <carter.joshua.603@gmail.com>
2024-06-25 00:26:17 -0700

    Reformat ./node-red.sh command and change container name to node-red-server

--- node-red.sh
+++ node-red.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-docker run --rm -it --add-host host.docker.internal.local=$(hostname -I | awk '{print $1}') -p 1880:1880 -v $(pwd)/packages/node-red-data:/data --name mynodered nodered/node-red:3.1.3-18
+
+docker run --rm -it \
+--name node-red-server \
+-p 1880:1880 \
+--add-host host.docker.internal.local=$(hostname -I | awk '{print $1}') \
+-v $(pwd)/packages/node-red-data:/data \
+ nodered/node-red:3.1.3-18
+
 read -n 1 -s -r -p "Press any key to continue"
