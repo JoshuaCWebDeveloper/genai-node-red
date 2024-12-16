33f8259952fb977da4bba27eae274c00714d657c
Joshua Carter <carter.joshua.603@gmail.com>
2024-12-15 22:17:48 -0800

    Implement new listener middleware pattern

--- packages/flow-client/src/app/redux/store.ts
+++ packages/flow-client/src/app/redux/store.ts
@@ -1,7 +1,6 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import {
-    flowApi } from './modules/api/flow.api';
     FLUSH,
     PAUSE,
     PERSIST,
@@ -11,9 +10,8 @@ import {
     REHYDRATE,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
 import type { AppLogic } from './logic';
-import { flowMiddleware } from './middleware/flow.middleware';
+import { flowApi } from './modules/api/flow.api';
 import { iconApi } from './modules/api/icon.api';
 import { nodeApi } from './modules/api/node.api'; // Import the nodeApi
 import {
@@ -27,11 +25,17 @@ import {
     FlowState,
 } from './modules/flow/flow.slice';
 import {
+    createRedListener,
+    startRedListener,
+} from './modules/flow/red.listener';
+import {
     PALETTE_NODE_FEATURE_KEY,
     paletteNodeReducer,
 } from './modules/palette/node.slice';
 
 export const createStore = (logic: AppLogic) => {
+    const redListener = createRedListener(logic);
+
     const store = configureStore({
         reducer: {
             [flowApi.reducerPath]: flowApi.reducer,
@@ -73,11 +77,13 @@ export const createStore = (logic: AppLogic) => {
                 nodeApi.middleware,
                 iconApi.middleware,
                 flowApi.middleware,
-                flowMiddleware),
+                redListener.middleware
+            ),
         devTools: process.env.NODE_ENV !== 'production',
     });
 
     setupListeners(store.dispatch);
+    startRedListener(redListener);
 
     return store;
 };
